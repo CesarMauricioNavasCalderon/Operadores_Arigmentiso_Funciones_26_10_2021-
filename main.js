ed00d4c204f968b20de7ef9e80385ec82c347396
cemanacajunio12345@gmail.com <cemanacajunio12345@gmail.com>
2021-10-26 22:05:40 -0500

    clase 26_10

---
+++ main.js
@@ -0,0 +1,66 @@
+let nombre = 'cesar mauricio navas calderon ';
+console.log(nombre);
+console.log(Object(nombre));
+
+
+
+
+let numero1 = 85
+let numero2 = 20
+
+
+
+let myStyle = `font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+color:#100808;
+font-weight: 500;
+border-radius: 10px;
+background:#9FE46A;
+padding: 5px;`;
+
+
+function suma(numero1, numero2, myStyle) {
+  let resultado = numero1 + numero2;
+console.group("Funcion SUMA");
+console.log(`%cEl numero1: N° ${numero1}`, myStyle);
+console.log(`%cEl numero2: N° ${numero2}`, myStyle);
+console.log(`%cResultado de la suma: ${resultado}`, myStyle);
+console.groupEnd();
+
+}
+
+suma(85,20,myStyle);
+
+
+function resta (numero1,numero2,myStyle) {
+    let resultado2 = numero1 - numero2;
+console.group  ("funcion RESTA")
+console.log(`%cEl numero1: N° ${numero1}`, myStyle);
+console.log(`%cEl numero2: N° ${numero2}`, myStyle);
+console.log(`%cResultado de la suma: ${resultado2}`, myStyle);
+console.groupEnd();
+}
+
+resta(85,20,myStyle)
+
+function multiplicacion ( numero1,numero2,myStyle){
+    let resultado3 = numero1 * numero2;
+console.group  ("funcion multiplicacion")
+console.log(`%cEl numero1: N° ${numero1}`, myStyle);
+console.log(`%cEl numero2: N° ${numero2}`, myStyle);
+console.log(`%cResultado de la suma: E° ${resultado3}`, myStyle);
+console.groupEnd();
+}
+
+multiplicacion(85,20,myStyle)
+
+
+function division ( numero1,numero2,myStyle) {
+    let resultado4 = numero1 / numero2;
+console.group  ("funcion division")
+console.log(`%cEl numero1: N° ${numero1}`, myStyle);
+console.log(`%cEl numero2: N° ${numero2}`, myStyle);
+console.log(`%cResultado de la suma: E° ${resultado4}`, myStyle);
+console.groupEnd();
+}
+
+division(85,20,myStyle)
